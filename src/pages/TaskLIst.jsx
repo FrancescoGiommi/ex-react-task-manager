8bed316696d7909e8763a97147ad8c4f157f881f
Francesco <you@example.com>
2025-03-23 19:23:31 +0100

    Add point 4 milestone 3 - function colorsStates

--- src/pages/TaskLIst.jsx
+++ src/pages/TaskLIst.jsx
@@ -3,6 +3,20 @@ import { TaskContext } from "../globalContext/TaskContext";
 import TaskRow from "../components/TaskRow";
 export default function TaskList() {
   const tasks = useContext(TaskContext);
+
+  // Funzione per colorare le righe in base allo stato
+  const colorsStates = (task) => {
+    if (task.status === "To do") {
+      return "bg-danger";
+    }
+    if (task.status === "Doing") {
+      return "bg-warning";
+    }
+    if (task.status === "Done") {
+      return "bg-success";
+    }
+    return "";
+  };
   return (
     <>
       <h1>Lista delle task</h1>
@@ -18,7 +32,7 @@ export default function TaskList() {
         </thead>
         <tbody>
           {tasks.map((task, id) => (
-            <TaskRow key={id} task={task} />
+            <TaskRow key={id} task={task} colorsStates={colorsStates} />
           ))}
         </tbody>
       </table>
