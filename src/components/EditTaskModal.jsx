eb1089887fa67355fd292388ed5ee099ed53cacd
Francesco <you@example.com>
2025-03-25 22:48:45 +0100

    Add milestone 10

--- src/components/EditTaskModal.jsx
+++ src/components/EditTaskModal.jsx
@@ -1,3 +1,67 @@
+import { useState, useRef } from "react";
+import Modal from "./Modal";
+
 export default function EditTaskModal({ show, onClose, task, onSave }) {
-  return;
+  const [editedTask, setEditedTask] = useState(task);
+  const editFormRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSave(editedTask);
+  };
+  const changeEditedTask = (key, event) => {
+    setEditedTask((prev) => ({
+      ...prev,
+      [key]: event.target.value,
+    }));
+  };
+
+  const { title, description, status } = editedTask;
+  return (
+    <Modal
+      title="Modifica task"
+      content={
+        <form ref={editFormRef} onSubmit={handleSubmit}>
+          <label>
+            Nome task:
+            <input
+              type="text"
+              className="form-control"
+              name="title"
+              value={title}
+              onChange={(e) => changeEditedTask("title", e)}
+            />
+          </label>
+          <label>
+            Descrizione:
+            <input
+              type="text"
+              className="form-control"
+              name="description"
+              value={description}
+              onChange={(e) => changeEditedTask("description", e)}
+            />
+          </label>
+          <label>
+            Stato:
+            <select
+              className="form-select form-control"
+              id="status"
+              name="status"
+              value={status}
+              onChange={(e) => changeEditedTask("status", e)}
+            >
+              <option value="To do">To do</option>
+              <option value="Doing">Doing</option>
+              <option value="Done">Done</option>
+            </select>
+          </label>
+        </form>
+      }
+      confirmText="Salva"
+      show={show}
+      onClose={onClose}
+      onConfirm={() => editFormRef.current.requestSubmit()}
+    />
+  );
 }
--- src/pages/TaskDetail.jsx
+++ src/pages/TaskDetail.jsx
@@ -2,6 +2,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import useTasks from "../customHooks/useTasks";
 import Modal from "../components/Modal";
+import EditTaskModal from "../components/EditTaskMOdal";
 
 export default function TaskDetail() {
   // Prendo i dati passati dalla pagina precedente
@@ -10,7 +11,8 @@ export default function TaskDetail() {
   // Mi permette di navigare tra le pagine
   const navigate = useNavigate();
 
-  const [show, setShow] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
 
   // Controllo se ci sono i dati della task
   if (!location.state || !location.state.task) {
@@ -25,24 +27,51 @@ export default function TaskDetail() {
 
   const { removeTask } = useTasks();
 
+  const updateTask = (updatedTask) => {
+    try {
+      fetch(`http://localhost:3001/tasks/${task.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedTask), // Usa l'oggetto aggiornato
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.success) {
+            alert("Task aggiornato con successo");
+            setShowUpdateModal(false);
+            navigate("/"); // Reindirizza dopo l'aggiornamento
+          } else {
+            alert("Errore nell'aggiornamento del task");
+          }
+        });
+    } catch {
+      (error) => console.error("Errore nell'aggiornamento del task:", error);
+    }
+  };
+
   const deleteTask = () => {
     // Rimuovo il task
-    fetch(`http://localhost:3001/tasks/${task.id}`, {
-      method: "DELETE",
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.success) {
-          removeTask(task.id);
-          alert("Task eliminato");
-          navigate("/");
-        } else {
-          alert("Errore nella cancellazione del task");
-        }
+    try {
+      fetch(`http://localhost:3001/tasks/${task.id}`, {
+        method: "DELETE",
       })
-      .catch((error) => {
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.success) {
+            removeTask(task.id);
+            alert("Task eliminato");
+            navigate("/");
+          } else {
+            alert("Errore nella cancellazione del task");
+          }
+        });
+    } catch {
+      (error) => {
         console.error("Errore nella cancellazione del task:", error);
-      });
+      };
+    }
   };
 
   return (
@@ -70,21 +99,35 @@ export default function TaskDetail() {
         </p>
       </div>
 
-      <button className="btn btn-danger" onClick={() => setShow(true)}>
+      <button
+        className="btn btn-danger me-2"
+        onClick={() => setShowDeleteModal(true)}
+      >
         Elimina task
       </button>
-
+      <button
+        className="btn btn-primary"
+        onClick={() => setShowUpdateModal(true)}
+      >
+        Modifica task
+      </button>
       {/* Modale per confermare la cancellazione */}
       <Modal
         title="Conferma Eliminazione"
         content="Sei sicuro di voler eliminare questa task?"
-        show={show}
-        onClose={() => setShow(false)}
+        show={showDeleteModal}
+        onClose={() => setShowDeleteModal(false)}
         onConfirm={() => {
           deleteTask();
-          setShow(false);
+          setShowDeleteModal(false);
         }}
         confirmText="Elimina"
+      />
+      <EditTaskModal
+        task={task}
+        show={showUpdateModal}
+        onClose={() => setShowUpdateModal(false)}
+        onSave={updateTask}
       />
     </>
   );
